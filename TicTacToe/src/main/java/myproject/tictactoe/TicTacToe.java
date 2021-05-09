d21ae5f4bd187513ea12b0c99e70dbe09c16349d
Hoàng Lưu <19020298@vnu.edu.vn>
2021-05-10 00:57:42 +0700

    TicTacToe V1
    no memu
    no socket

---
+++ TicTacToe/src/main/java/myproject/tictactoe/TicTacToe.java
@@ -0,0 +1,114 @@
+package myproject.tictactoe;
+
+import java.awt.Color;
+import javax.swing.JButton;
+
+public class TicTacToe {
+    public int COUNT = 0;
+    public int width = 20, height = 20, turn = 0; //size of board
+    public JButton box[][] = new JButton[height][width];
+    public String chatString = ""; //save all chat in in string and show in chatField
+    int pos[][] = new int[height][width];
+    
+    /**
+     * 
+     * @param x coordinate of box
+     * @param y coordinate of box
+     * @param boxName (X or O)
+     * @return true if win
+     */
+    public boolean checkWin(int x, int y, String boxName) {  
+        int i, j;
+        int d = 0;
+        //check vertical
+        for (i = -4; i <= 4; i++) {
+            if (x + i >= 0 && x + i < height) {
+                if (box[x + i][y].getText() == boxName) {
+                    d++;
+                } else if (d < 5) {
+                    d = 0;
+                }
+            }
+        }
+        if (d >= 5) {
+            for (i = -4; i <= 4; i++) {
+                if (x + i >= 0 && x + i < height) {
+                    if (box[x + i][y].getText() == boxName) {
+                        box[x + i][y].setBackground(Color.yellow);
+                    }
+                }
+            }
+            return true;
+        } else {
+            d = 0;
+        }
+        //check horizontal
+        for (i = -4; i <= 4; i++) {
+            if (y + i >=0 && y + i < width) {
+                if (box[x][y + i].getText() == boxName) {
+                    d++;
+                } else if (d < 5) {
+                    d = 0;
+                }
+            }
+        }
+        if (d >= 5) {
+            for (i = -4; i <= 4; i++) {
+                if (y + i >= 0 && y + i < height) {
+                    if (box[x][y + i].getText() == boxName) {
+                        box[x][y + i].setBackground(Color.yellow);
+                    }
+                }
+            }
+            return true;
+        } else {
+            d = 0;
+        }
+        //check diagonal
+        for (i = -4, j = 4; i <= 4 && j >= -4; i++, j--) {
+            if(y + i >= 0 && y + i < width && x + j >= 0 && x + j < height) {
+                if (box[x + j][y + i].getText() == boxName) {
+                    d++;
+                } else if (d < 5) {
+                    d = 0;
+                }
+            } 
+        }
+        if (d >= 5) {
+            for (i = -4, j = 4; i <= 4 && j >= -4; i++, j--) {
+                if(y + i >= 0 && y + i < width && x + j >= 0 && x + j < height) {
+                    if (box[x + j][y + i].getText() == boxName) {
+                        box[x + j][y + i].setBackground(Color.yellow);
+                    }
+                } 
+            }
+            return true;
+        } else {
+            d = 0;
+        }
+        for (i = -4; i <=4 ; i++) {
+            if (x + i >= 0 && x + i < width && y + i >= 0 && y + i < height) {
+                if (box[x + i][y + i].getText() == boxName) {
+                    d++;
+                } else if (d < 5) {
+                    d = 0;
+                }
+            }
+        }
+        if (d >= 5) {
+            for (i = -4; i <=4 ; i++) {
+                if (x + i >= 0 && x + i < width && y + i >= 0 && y + i < height) {
+                    if (box[x + i][y + i].getText() == boxName) {
+                        box[x + i][y + i].setBackground(Color.yellow);
+                    }
+                }
+            }
+            return true;
+        } else {
+            d = 0;
+        }
+        return false;
+    }
+    
+    
+}
